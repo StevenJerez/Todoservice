f76712a47575f8bcfc7c9fb05311656cae2c655b
Steve <tetraman4arms@gmail.com>
2018-01-02 02:50:01 -0400

    new view
    agregamos la vista de recomendaciones

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-$(".circles-item").click(function () {
-  $(this).toggleClass('active');
-  if ($(".circles-item").hasClass('active')) {
-    $(".btn-next").addClass('show');
-  } else {
-    $(".btn-next").removeClass('show');
-  }
+$(document).ready(function() {
+  $(".circles-item").click(function () {
+    $(this).toggleClass('active');
+    if ($(".circles-item").hasClass('active')) {
+      $(".btn-next").addClass('show');
+    } else {
+      $(".btn-next").removeClass('show');
+    }
+  });
+  $('.interest-cards').masonry({
+  itemSelector: '.interest-cards__item',
+  columnWidth: 268,
+  gutter: 16
+});
 });
